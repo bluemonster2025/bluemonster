e61327ea5eadc194bb7f32f366af83801c00f4f6
cheillaamelo <cheillaaaa@gmail.com>
2025-09-09 14:39:52 -0300

    fix

--- components/layouts/LPLayout/Footer/index.tsx
+++ components/layouts/LPLayout/Footer/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Icon from "@/components/elements/Icon";
 import { LinkExternal } from "@/components/elements/LinkExternal/LinkExternal";
 import { Section } from "@/components/elements/Section";
@@ -9,24 +11,26 @@ import { WhatsAppButton } from "@/components/elements/WhatsAppButton";
 export const Footer = () => {
   return (
     <>
+      {/* Parte superior do Footer */}
       <Section className="border-b border-white bg-purplescale-50">
         <div className="flex flex-col sm:flex-row gap-4 items-center justify-between py-4">
-          <Link href="/" className="relative w-45 h-12 flex">
+          {/* Logo */}
+          <Link
+            href="/"
+            className="relative w-45 h-12 flex"
+            aria-label="Página inicial Bluemonster"
+          >
             <Image
               fill
-              alt="Bluemonster"
+              alt="Logo Bluemonster"
               src="/images/logos/logo-branco.svg"
               style={{ objectFit: "contain" }}
               priority
             />
           </Link>
+
+          {/* Redes sociais */}
           <div className="grid gap-1 grid-cols-1">
-            {/* <LinkExternal
-              className="h-10 w-10 flex items-center justify-center rounded bg-white"
-              href="/"
-            >
-              <Icon name="FaLinkedinIn" color="#687af6" size={16} />
-            </LinkExternal> */}
             <LinkExternal
               className="h-10 w-10 flex items-center justify-center rounded bg-white"
               href="https://www.instagram.com/bluemonster.com.br"
@@ -37,17 +41,17 @@ export const Footer = () => {
           </div>
         </div>
       </Section>
+
+      {/* Parte central com chamada para ação */}
       <Section className="bg-purplescale-50">
         <div className="hidden md:flex lg:items-center justify-between py-8">
           <div className="lg:pr-20">
             <Text className="text-white text-left text-base/[24px] font-semibold">
-              <div>Transforme sua ideia em um site hoje mesmo 🚀</div>
+              Transforme sua ideia em um site hoje mesmo 🚀
             </Text>
             <Text className="text-white text-left text-base/[24px] font-light">
-              <div>
-                Entre em contato conosco e solicite um orçamento para o seu
-                projeto.
-              </div>
+              Entre em contato conosco e solicite um orçamento para o seu
+              projeto.
             </Text>
           </div>
 
@@ -57,6 +61,8 @@ export const Footer = () => {
           </div>
         </div>
       </Section>
+
+      {/* Parte inferior do Footer */}
       <Section>
         <Text className="text-center lg:text-left py-4 text-sm/[24px] text-grayscale-400 w-full h-full font-normal">
           ©2025 Todos os direitos reservados bluemonster.com.br
