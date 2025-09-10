25696e4cda00b048a65e279e9b124aa6df9c20d7
cheillaamelo <cheillaaaa@gmail.com>
2025-09-09 22:08:44 -0300

    fix

--- components/elements/LinkExternal/LinkExternal.tsx
+++ components/elements/LinkExternal/LinkExternal.tsx
@@ -1,17 +1,23 @@
-import { ReactNode } from 'react'
+import { ReactNode } from "react";
 
 export interface Props
   extends React.DetailedHTMLProps<
     React.AnchorHTMLAttributes<HTMLAnchorElement>,
     HTMLAnchorElement
   > {
-  children: ReactNode
+  children: ReactNode;
 }
 
-export const LinkExternal = ({ children, className, href }: Props) => {
+export const LinkExternal = ({ children, className, href, ...rest }: Props) => {
   return (
-    <a target="_blank" rel="noopener noreferrer" {...{ href, className }}>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+      className={className}
+      {...rest}
+    >
       {children}
     </a>
-  )
-}
+  );
+};
