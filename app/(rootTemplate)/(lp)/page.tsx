1c1ce3c76d49fe62531296eb980ea6292a4ea238
cheillaamelo <cheillaaaa@gmail.com>
2025-09-11 21:18:19 -0300

    fix

--- app/(rootTemplate)/(lp)/page.tsx
+++ app/(rootTemplate)/(lp)/page.tsx
@@ -3,14 +3,21 @@ import { LPTemplate } from "@/components/templates/LPTemplate";
 export const metadata = {
   title: "Blue Monster",
   description:
-    "Crie um site exclusivo e gerenciável para o seu negócio – com design moderno, loja virtual, SEO otimizado, hospedagem e suporte completo. Solicite um orçamento e aumente suas vendas hoje mesmo!",
+    "Crie um site exclusivo e gerenciável para o seu negócio - com design moderno, loja virtual, SEO otimizado, hospedagem e suporte completo. Solicite um orçamento e aumente suas vendas hoje mesmo!",
   openGraph: {
     url: "https://bluemonster.com.br/",
     type: "website",
     title: "Blue Monster",
     description:
       "Crie um site exclusivo e gerenciável para o seu negócio - com design moderno, loja virtual, SEO otimizado, hospedagem e suporte completo. Solicite um orçamento e aumente suas vendas hoje mesmo!",
-    images: "/images/pictures/thumbnail.webp",
+    images: [
+      {
+        url: "https://bluemonster.com.br/images/pictures/thumbnail.jpg", // URL ABSOLUTA
+        width: 1200,
+        height: 630,
+        alt: "Blue Monster",
+      },
+    ],
   },
 };
 
