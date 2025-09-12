83e84161348cfe137db82d4d57e6109a2d0b4f34
cheillaamelo <cheillaaaa@gmail.com>
2025-09-11 21:28:35 -0300

    fix

--- app/(rootTemplate)/(lp)/page.tsx
+++ app/(rootTemplate)/(lp)/page.tsx
@@ -12,7 +12,7 @@ export const metadata = {
       "Crie um site exclusivo e gerenciável para o seu negócio - com design moderno, loja virtual, SEO otimizado, hospedagem e suporte completo. Solicite um orçamento e aumente suas vendas hoje mesmo!",
     images: [
       {
-        url: "https://bluemonster.com.br/images/pictures/thumbnail.jpg", // URL ABSOLUTA
+        url: "https://bluemonster.com.br/images/pictures/thumbnail-new.jpg", // URL ABSOLUTA
         width: 1200,
         height: 630,
         alt: "Blue Monster",
